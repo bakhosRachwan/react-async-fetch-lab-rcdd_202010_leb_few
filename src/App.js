b480ca5d12c6268dd01a3d3f083873f50491d6a6
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-12 17:08:17 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-// create your App component here
+import React from 'react'
+
+class App extends Reacr.Component{
+  componentDidMount(){
+    fetch(``)
+    .then(res => res.json())
+    .then(data => data.result)
+  }
+  render(){
+    return(null)
+  }
+}
+export default App
